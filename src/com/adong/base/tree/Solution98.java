0d4fa208ad91e38adcb56822dabce013795de6b0
beiting <beiting@wacai.com>
2022-09-09 00:04:58 +0800

    mark

---
+++ src/com/adong/base/tree/Solution98.java
@@ -0,0 +1,9 @@
+package com.adong.base.tree;
+
+/**
+ * @author beiting
+ * @Description Solution98
+ * @Date 2022-09-08 5:49 PM
+ */
+public class Solution98 {
+}
---
+++ src/com/adong/base/tree/TreeNode.java
@@ -0,0 +1,9 @@
+package com.adong.base.tree;
+
+/**
+ * @author beiting
+ * @Description TreeNode
+ * @Date 2022-09-08 5:50 PM
+ */
+public class TreeNode {
+}
